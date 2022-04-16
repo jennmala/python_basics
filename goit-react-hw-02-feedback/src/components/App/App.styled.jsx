64c12badcce20e45055e6bc2bb1c909d25e0219f
Yevheniia Malakhova <88288408+jennmala@users.noreply.github.com>
2022-04-16 11:52:09 +0300

    Delete goit-react-hw-02-feedback directory

--- goit-react-hw-02-feedback/src/components/App/App.styled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  padding-top: 10px;
-  padding-bottom: 10px;
-  background-color: #dddddd;
-`;
--- goit-react-hw-02-feedback/src/components/FriendList/FriendList.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const FriendListWrap = styled.ul`
-  padding: 0;
-  width: 400px;
-  margin: 0 auto;
-  margin-bottom: 30px;
-  list-style: none;
-`;
